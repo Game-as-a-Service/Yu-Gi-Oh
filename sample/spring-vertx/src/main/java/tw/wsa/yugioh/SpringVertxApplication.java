1fe17444fda0d21d3344ba8be7b38679cc5caabb
Vincent Huang MacBook <yfr.huang@hotmail.com>
2022-10-22 01:13:40 +0800

    :tada: feat: init commit

---
+++ sample/spring-vertx/src/main/java/tw/wsa/yugioh/SpringVertxApplication.java
@@ -0,0 +1,13 @@
+package tw.wsa.yugioh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVertxApplication.class, args);
+    }
+
+}
---
+++ sample/spring-vertx/src/test/java/tw/wsa/yugioh/SpringVertxApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.wsa.yugioh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVertxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
